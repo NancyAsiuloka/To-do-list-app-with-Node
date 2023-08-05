f7679e6a852f8adeb0ba208e4ae5147110126abf
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-05 20:33:09 +0100

    added controller.js file

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,4 +4,12 @@ module.exports = function(app){
     app.get('/todo', function(req, res){
 
     });
+
+    app.post('/todo', function(req, res){
+
+    });
+
+    app.delete('/todo', function(req, res){
+
+    });
 };
