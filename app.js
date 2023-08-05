ed6c022ed42a4837d7f50ccdf0eb8d1f087ae49d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-05 20:32:14 +0100

    created a fire controller

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 
+var todoController = require('./controllers/todoController');
+
+
 var app = express();
 
 // set up template engine
@@ -7,6 +10,9 @@ app.set('view engine', 'ejs');
 
 // static files(middleware)
 app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
 
 // listen to port
 app.listen(3000);
