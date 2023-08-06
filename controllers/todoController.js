3f718cac46408bf0e9d4fa54695c274d319e8281
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-06 13:18:20 +0100

    added some dummy data

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,12 @@
+var data = [{
+    item: 'get milk'
+}, {item: 'work dog'}, {item: 'kick some coding ass'}];
+
 module.exports = function(app){
 
 
     app.get('/todo', function(req, res){
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
     app.post('/todo', function(req, res){
