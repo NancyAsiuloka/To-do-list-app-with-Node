585241b58fa44b8c6d1c8d3e82c1e01ad3ba0671
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-07 14:39:52 +0100

    made some changes in the app.js file

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var express = require('express');
-
 var todoController = require('./controllers/todoController');
-
 
 var app = express();
 
-// set up template engine
+// Set up template engine
 app.set('view engine', 'ejs');
 
-// static files(middleware)
+// Static files (middleware)
 app.use(express.static('./public'));
 
-// fire controllers
+// Fire controllers
 todoController(app);
 
-// listen to port
+// Listen to port
 app.listen(3000);
 console.log('You are listening to port 3000');
-
