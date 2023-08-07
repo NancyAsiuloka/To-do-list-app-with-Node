03e376832eaa9f4f05afbc53476f132857f0aede
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-07 14:38:43 +0100

    renamed the todo controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,19 +1,26 @@
-var data = [{
-    item: 'get milk'
-}, {item: 'work dog'}, {item: 'kick some coding ass'}];
+var bodyParser = require('body-parser');
 
-module.exports = function(app){
+var data = [
+  { item: 'get milk' },
+  { item: 'work dog' },
+  { item: 'kick some coding ass' }
+];
 
+var urlencodedparser = bodyParser.urlencoded({ extended: false });
 
-    app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
-    });
+module.exports = function (app) {
+  app.get('/todo', function (req, res) {
+    res.render('todo', { todos: data });
+  });
 
-    app.post('/todo', function(req, res){
+  app.post('/todo', urlencodedparser, function (req, res) {
+    var newItem = req.body.item;
+    data.push({ item: newItem });
+    console.log(newItem)
+    res.json(data);
+  });
 
-    });
-
-    app.delete('/todo', function(req, res){
-
-    });
+  app.delete('/todo', function (req, res) {
+    // Implement delete functionality if needed
+  });
 };
