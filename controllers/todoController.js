72e00aab1a26b776fad7b15c40a097ca33bbd3fe
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-09 15:04:24 +0100

    added  a comment

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,25 +1,29 @@
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 var data = [
-  { item: 'get milk' },
-  { item: 'work dog' },
-  { item: 'kick some coding ass' }
+  { item: "get milk" },
+  { item: "work dog" },
+  { item: "kick some coding ass" },
 ];
 
 var urlencodedparser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
-  app.get('/todo', function (req, res) {
-    res.render('todo', { todos: data });
+  app.get("/todo", function (req, res) {
+    res.render("todo", { todos: data });
   });
 
-  app.post('/todo', urlencodedparser, function (req, res) {
+  app.post("/todo", urlencodedparser, function (req, res) {
     var newItem = req.body.item;
     data.push({ item: newItem });
-
+    console.log(newItem);
+    res.json(data);
   });
 
-  app.delete('/todo', function (req, res) {
+  app.delete("/todo/:item", function (req, res) {
     // Implement delete functionality if needed
+    data = data.filter(function(todo){
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    })
   });
 };
